d60153f8923ebcd754af8d6bf2547338de53fc52
will <contatowilliamsj@gmail.com>
2024-01-22 20:05:14 -0300

    create: first commit

---
+++ src/data-source.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import 'reflect-metadata'
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT as number | undefined
+})
