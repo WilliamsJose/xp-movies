42120fc4adcfcde852bed4f7c27fad8454b55a16
will <contatowilliamsj@gmail.com>
2024-01-23 17:29:06 -0300

    feat: encrypted password, login w/out token and all user favorites movies

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,19 +1,35 @@
 import { Request, Response } from "express"
 import { userRepository } from "../repositories/userRepository"
 import { userFavoriteRepository } from "../repositories/userFavoriteRepository"
+import bcrypt from "bcrypt"
+import { validate } from "email-validator";
+
+function isEmpty(...args: any[]) {
+  args.forEach((element: any) => {
+    if (String(element).trim() === '' ) {
+      return true
+    }
+  });
+  return false
+}
 
 export class UserController {
   async create(req: Request, res: Response) {
     const { name, email, password } = req.body
     
-    // TODO implement a cool validator
     if (!name || !email || !password) {
       return res.status(400).json({ message: 'You must provide a name, email and password.' })
     }
 
+    if (!validate(email)) {
+      return res.status(400).json({ message: 'Email is not valid' })
+    }
+
     try {
+      const encryptedPass = await bcrypt.hash(password, 10)
+
       const newUser = userRepository.create({
-        name, email, password
+        name, email, password: encryptedPass
       })
 
       await userRepository.save(newUser)
@@ -38,7 +54,7 @@ export class UserController {
         return res.status(404).json({ message: 'You must be registered to add an favorite.' })
       }
       if (!imdbId || !category || !movieName) {
-        return res.status(404).json({ message: 'You must provide an imdbId, category and movieName.' })
+        return res.status(400).json({ message: 'You must provide an imdbId, category and movieName.' })
       }
 
       const newFavorite = userFavoriteRepository.create({
@@ -56,4 +72,39 @@ export class UserController {
       return res.status(500).json({ message: error.detail })
     }
   }  
+
+  async findAllUserFavorites(req: Request, res: Response) {
+    const { userId } = req.params
+
+    try {
+      const userFavorites = await userFavoriteRepository.findBy({ user: {
+        id: Number(userId)
+      } })
+
+      res.status(200).json({ userFavorites })
+
+    } catch (error: any) {
+      return res.status(500).json({ message: error.detail })
+    }
+  }
+
+  async login(req: Request, res: Response) {
+    const { email, password } = req.body
+
+    try {
+      
+      const userFound = await userRepository.findOneByOrFail({ email })
+      
+      const successLogin = await bcrypt.compare(password, userFound.password)
+
+      if (successLogin) {
+        return res.status(200).json({ message: 'User Logged' })
+      } else {
+        return res.status(400).json({ message: 'Wrong Email or Password' })
+      }
+
+    } catch (error: any) {
+      return res.status(500).json({ message: error.message })
+    }
+  }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { UserController } from "./controllers/UserController";
 
 const routes = Router()
 
+routes.get('/favorites/:userId/all', new UserController().findAllUserFavorites)
+routes.post('/login', new UserController().login)
 routes.post('/user', new UserController().create)
 routes.post('/user/:userId/addFavorite', new UserController().addFavorite)
 
