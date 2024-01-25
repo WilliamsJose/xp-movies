f0548fcd39ba471613261decb08ecbb79f791229
will <contatowilliamsj@gmail.com>
2024-01-25 17:55:01 -0300

    feat: refresh token working like a charm

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken"
 import { Request, Response, NextFunction } from "express"
+import { userTokenRepository } from "../repositories/userTokenRepository";
 
 declare module 'express' {
   interface Request {
@@ -18,5 +19,30 @@ export async function verifyToken(req: Request, res: Response, next: NextFunctio
     next()
   } catch (error) {
     res.status(401).json({ message: 'Invalid token' })
+  }
+}
+
+export async function refreshToken(req: Request, res: Response) {
+  const refreshToken = req.headers['refreshtoken']
+
+  try {
+    
+    if (!refreshToken) {
+      return res.status(403).json({ message: 'No refresh token provided.' })  
+    }
+
+    // is token valid?
+    const decodedUserToken: any = jwt.verify(String(refreshToken), process.env.JWT_SECRET || "")
+
+    // token exists on database?
+    await userTokenRepository.findOneByOrFail({ refresh_token: String(refreshToken) })
+
+    const newAccessToken = jwt.sign({ id: decodedUserToken.id }, process.env.JWT_SECRET || "", {
+      expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
+    });
+
+    return res.status(200).header('Authorization', newAccessToken).json({ message: 'New access token generated' })
+  } catch (error: any) {
+    return res.status(500).json({ message: error.detail })
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
-import { verifyToken } from "./middleware/authMiddleware";
+import { refreshToken, verifyToken } from "./middleware/authMiddleware";
 
 const routes = Router()
 
 routes.get('/favorites/all', verifyToken, (req, res) => new UserController().findAllUserFavorites(req, res))
+routes.get('/refreshToken', refreshToken)
 routes.post('/login', new UserController().login)
 routes.post('/register', new UserController().register)
 routes.post('/user/addFavorite', verifyToken, (req, res) => new UserController().addFavorite(req, res))
