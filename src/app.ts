be31f5697dc1be2fb68c1e40ef5466d9f4bb9808
will <contatowilliamsj@gmail.com>
2024-02-11 18:39:26 -0300

    feat: add cors and rename server to app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import { AppDataSource } from './data-source'
 import routes from './routes'
+import cors from 'cors'
 
 AppDataSource.initialize().then(() => {
   const app = express()
 
   app.use(express.json())
+  app.use(cors())
   app.use(routes)
 
+  // eslint-disable-next-line no-console
   return app.listen(process.env.PORT, () => console.log('server up'))
 })
