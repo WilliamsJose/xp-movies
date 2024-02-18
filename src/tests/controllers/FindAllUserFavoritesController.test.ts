e9bfcea2f499df9540522d7964332c99c11ba283
will <contatowilliamsj@gmail.com>
2024-02-18 16:57:58 -0300

    test: fix way to correct unit testing

--- src/tests/controllers/FindAllUserFavoritesController.test.ts
+++ src/tests/controllers/FindAllUserFavoritesController.test.ts
@@ -1,21 +1,18 @@
 import { FindAllUserFavoritesController } from '../../controllers'
 import { IUseCase } from '../../domains/useCases/IUseCase'
-import { UseCaseResponsesEnum } from '../../enums/UseCaseResponsesEnum'
 
 describe('FindAllUserFavoritesController: testing all responses', () => {
   it('should call execute and return apropriate http response with no favorites', async () => {
     const userId = '123'
     const request = { query: { userId } }
-    const useCaseResult = { code: UseCaseResponsesEnum.Success, body: { favorites: [] }, headers: undefined }
     const findAllUserFavoriteUseCaseMock: IUseCase = {
-      execute: jest.fn().mockResolvedValue(useCaseResult)
+      execute: jest.fn()
     }
 
     const controller = new FindAllUserFavoritesController(findAllUserFavoriteUseCaseMock)
-    const result = await controller.handle(request)
+    await controller.handle(request)
 
     expect(findAllUserFavoriteUseCaseMock.execute).toHaveBeenCalledWith(userId)
-    expect(result).toEqual({ status: 200, headers: undefined, body: { favorites: [] } })
   })
 
   it('should call execute and return internal server error http response', async () => {
