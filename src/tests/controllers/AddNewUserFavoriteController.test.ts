3c53dd3717a330059a736da716a66c5ad4658913
will <contatowilliamsj@gmail.com>
2024-02-18 17:07:10 -0300

    test: good behavior

---
+++ src/tests/controllers/AddNewUserFavoriteController.test.ts
@@ -0,0 +1,23 @@
+import { AddNewUserFavoriteController } from '../../controllers'
+
+describe('AddNewUserFavoriteController:', () => {
+  it('should call execute with correct parameters', async () => {
+    const request = {
+      query: { userId: 1 },
+      body: { imdbId: 'tt123', categoriesIds: [1], title: 'the movie' }
+    }
+    const addNewUserFavoriteUseCaseMock = {
+      execute: jest.fn()
+    }
+
+    const controller = new AddNewUserFavoriteController(addNewUserFavoriteUseCaseMock)
+    await controller.handle(request)
+
+    expect(addNewUserFavoriteUseCaseMock.execute).toHaveBeenCalledWith(
+      request.query.userId,
+      request.body.imdbId,
+      request.body.categoriesIds,
+      request.body.title
+    )
+  })
+})
